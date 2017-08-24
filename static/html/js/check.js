95e91172f14c9f15d5ae85b08cd934802d5b5922
Benjamin Keller <benjamin.keller@analytical-software.de>
2017-08-24 15:19:43 +0200

    adapted client checker to pass GET parameter through

--- static/html/js/check.js
+++ static/html/js/check.js
@@ -3,9 +3,13 @@ $(function() {
   var redirectIfClientIsAvailable = function(eidClient) {
     $('#result').html('Detected eID-Client. Forwarding...');
     setTimeout(function() {
-      window.location = "../api/getTcTokenUrl?protocol=" + eidClient.protocol + "&host=" + eidClient.host;
+      window.location = "../api/eIdService/getTcTokenUrl?protocol=" + eidClient.protocol + "&host=" + eidClient.host + "&" + window.location.search.substr(1);
     }, 3000);
   };
 
   var checker = new ClientChecker(redirectIfClientIsAvailable, 5000);
+
+  $('#continueButton').click(function() {
+      window.location = "../api/eIdService/getTcTokenUrl?" + window.location.search.substr(1);
+  });
 });
