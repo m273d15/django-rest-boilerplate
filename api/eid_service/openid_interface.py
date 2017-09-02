6de50e5f1943279720d79b0f21de6f3789bc79d1
Niels Groth <niels.groth@grothbande.de>
2017-09-02 13:14:03 +0200

    fix catch validation error

--- api/eid_service/openid_interface.py
+++ api/eid_service/openid_interface.py
@@ -1,14 +1,15 @@
 from uuid import UUID
+from django.core.exceptions import ValidationError
 from eid_service.models import AuthenticationRequest
 
 
 def get_user_id(accessToken):
     """
-    Given a valid access token this view returns the user id retieved during an
+    Given a valid access token this view returns the user id retrieved during an
     authentication session between eID-Client and eID-Server.
     """
     try:
         authenticationRequest = AuthenticationRequest.objects.get(accessToken=accessToken)
-    except AuthenticationRequest.DoesNotExist:
+    except (AuthenticationRequest.DoesNotExist, ValidationError):
         return None
     return str(authenticationRequest.restrictedId)
