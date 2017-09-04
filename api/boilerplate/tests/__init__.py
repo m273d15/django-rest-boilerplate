95cf72f7465a19e6a78afc100bb11a277625b496
Larissa Zech <larissa.zech@fu-berlin.de>
2017-09-04 21:25:15 +0200

    Implement PersonalDataType

--- api/boilerplate/tests/__init__.py
+++ api/boilerplate/tests/__init__.py
@@ -3,4 +3,5 @@ from boilerplate.tests.hexbinary import *
 from boilerplate.tests.restricted_id import *
 from boilerplate.tests.place import *
 from boilerplate.tests.generaldate import *
+from boilerplate.tests.personaldata import *
 
